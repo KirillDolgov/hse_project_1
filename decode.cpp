17f29081fb43932902b82d7c5b82fe4cf68b281f
KirillDolgov <kir.iv.dol@gmail.com>
2025-02-15 09:46:08 +0300

    encode decode

---
+++ decode.cpp
@@ -0,0 +1,148 @@
+#include "node.hpp"
+
+#include <fstream>
+#include <iostream>
+#include <bitset>
+#include <algorithm>
+
+#include <queue>
+#include <vector>
+
+using namespace std;
+using queue_t = priority_queue<Node::pointer, vector<Node::pointer>, LowestPriority>;
+using uchar = unsigned char;
+
+void read_decoding_file(string filename, vector<int>& freq, string& message){
+    string new_filename = filename + ".hfc";
+    ifstream ifs(new_filename, ifstream::binary);
+    if(!ifs){
+        return;
+    }
+
+    uchar count = 0;
+    ifs.read(reinterpret_cast<char*>(&count), sizeof count);
+    // cout << "Read count: " << (int)count << endl;
+    int i = 0;
+    while(i < count){
+        uchar ch;
+        ifs.read(reinterpret_cast<char*>(&ch), sizeof ch);
+
+
+        int f = 0;
+        ifs.read(reinterpret_cast<char*>(&f), sizeof f);
+        freq[ch] = f;
+        ++i;
+    }
+
+    // for_each(freq.begin(), freq.end(), [index = uchar(0)] (const int& value) mutable {
+    //     if(value != 0){
+    //         cout << 
+    //     }
+    // });
+
+    int byte_count = 0;
+    uchar modulo = 0;
+    ifs.read(reinterpret_cast<char*>(&byte_count), sizeof byte_count);
+    ifs.read(reinterpret_cast<char*>(&modulo), sizeof modulo);
+
+    int i = 0;
+    for(; i < byte_count; ++i){
+        uchar byte;
+        ifs.read(reinterpret_cast<char*>(&byte), sizeof byte);
+        bitset<CHAR_BIT> b(byte);
+        message += b.to_string();
+    }
+    if(modulo > 0){
+        uchar byte;
+        ifs.read(reinterpret_cast<char*>(&byte), sizeof byte);
+        bitset<CHAR_BIT> b(byte);
+        message += b.to_string().substr(CHAR_BIT - modulo, CHAR_BIT);
+    }
+}
+
+void make_char(const Node::pointer& root, const string& message, string& text){
+    Node::pointer node = root;
+    for(size_t i = 0; i < message.size(); ++i){
+        char ch = message[i];
+        if(ch == '0'){
+            if(node->left != nullptr){
+                node = node->left;
+                if(node->left == nullptr && node->right == nullptr){
+                    text += node->get_byte();
+                    node = root;
+                }
+            }
+        } else if (ch == '1'){
+            if(node->right != nullptr){
+                node = node->right;
+                if(node->left == nullptr && node->right == nullptr){
+                    text += node->get_byte();
+                    node = root;
+                }
+            }    
+        }
+        
+    }
+
+}
+
+void write_decoding_file(const string& filename, const string& text){
+    string new_filename = "decoded_" + filename;
+    ofstream ofs(new_filename, ofstream::binary);
+    if(!ofs){
+        return;
+    }
+
+    ofs << text;
+
+}
+
+
+
+
+
+int main(){
+    
+    string frname;
+    cout << "Введите имя файла без \".hfs\": ";
+    cin >> frname;
+
+    vector<int> freq2(256, 0);
+    string message2 = "";
+
+    read_decoding_file(frname, freq2, message2);
+
+
+
+    queue_t queue2;
+    
+    for(int i = 0; i < 256; ++i){
+        if(freq[i]!=0){
+            Node::pointer node = make_shared<Node>(i, freq2[i]);
+            queue.push(node);
+        }
+    }
+
+
+    while(queue2.size() > 1)
+    {
+    Node::pointer x = queue2.top();
+    queue.pop();
+    Node::pointer y = queue2.top();
+    queue.pop();
+    Node::pointer z = make_shared<Node>(0, x->get_freq()+y->get_freq());
+    z->left = x;
+    z->right = y;
+    x->parent = z;
+    y->parent = z;
+    queue2.push(z);
+    } 
+
+    
+    Node::pointer root2 = queue2.top();
+    string text = "";
+    make_char(root2, message2, text);
+    write_decoding_file(frname, text);
+
+    return 0;
+}
---
+++ encode.cpp
@@ -0,0 +1,147 @@
+#include "node.hpp"
+
+#include <fstream>
+#include <iostream>
+#include <bitset>
+#include <algorithm>
+
+#include <queue>
+#include <vector>
+
+using namespace std;
+using queue_t = priority_queue<Node::pointer, vector<Node::pointer>, LowestPriority>;
+using uchar = unsigned char;
+
+void make_code(Node::pointer& node, string str, vector<string>& codes){
+    if(node->left != nullptr){
+        make_code(node->left, str + "0", codes);
+    }
+    if(node->right != nullptr){
+        make_code(node->right, str + "1", codes);
+    }
+
+    if(node->left == nullptr && node->right == nullptr){
+        node->code(str);
+        codes[node->get_byte()] = str;
+        // cout << "leaf: " << (int)node->get_byte() << " code: " << node->code() << "\n";
+    }
+}
+
+
+string message2code(const string& filename, const vector<string>& codes) {
+    FILE* file = fopen(filename.c_str(), "rb");
+    if (!file) {
+        cerr << "Ошибка: не удалось открыть файл.\n";
+        return "";
+    }
+
+    string encoded_string {""};
+    int ch;
+    while ((ch = fgetc(file)) != EOF) {
+        encoded_string += codes[(unsigned char)ch];
+    }
+    
+    fclose(file);
+    return encoded_string;
+}
+
+
+void write_file(const string& filename, vector<int>& freq, const queue_t& queue, const string& message){
+    string new_filename = filename + ".hfc";
+    ofstream ofs(new_filename, ofstream::binary);
+    if(!ofs){
+        return;
+    }
+
+    unsigned char count = count_if(freq.begin(), freq.end(), [](const int& value) { return (value != 0); });
+    cout << "Count: " << (int)count << endl;
+
+    ofs.write(reinterpret_cast<char*>(&count), sizeof count);
+
+    for_each(freq.begin(), freq.end(), [index=uchar(0), &ofs](int& value) mutable {
+        if(value != 0){
+            ofs.write(reinterpret_cast<char*>(&index), sizeof index);
+            ofs.write(reinterpret_cast<char*>(&value), sizeof value);
+        }
+        ++index;
+    });
+
+    int byte_count = message.size() / 8;
+    unsigned char modulo = message.size() % 8;
+    ofs.write(reinterpret_cast<char*>(&byte_count), sizeof byte_count);
+    ofs.write(reinterpret_cast<char*>(&modulo), sizeof modulo);
+
+    int i = 0;
+    for(; i<byte_count; ++i){
+        bitset<CHAR_BIT> b(message.substr(i * CHAR_BIT, CHAR_BIT));
+        unsigned char value = static_cast<unsigned char>(b.to_ulong());
+        ofs.write(reinterpret_cast<char*>(&value), sizeof value);
+    }
+    if(modulo > 0){
+        bitset<CHAR_BIT> b(message.substr(i * CHAR_BIT, modulo));
+        unsigned char value = static_cast<unsigned char>(b.to_ulong());
+        ofs.write(reinterpret_cast<char*>(&value), sizeof value);
+    }
+
+}
+
+
+
+
+int main(){
+    string frname;
+    cout << "Введите имя файла: ";
+    cin >> frname;
+
+    vector<int> freq(256, 0);
+
+    FILE* fr = fopen(frname.c_str(), "rb");
+    if (!fr) {
+        cerr << "Ошибка: не удалось открыть файл.\n";
+        return 1;
+    }
+
+    int ch;
+    while ((ch = fgetc(fr)) != EOF) {
+        freq[(unsigned char)ch]++;
+    }
+    fclose(fr);
+
+
+    queue_t queue;
+
+    for(int i = 0; i < 256; ++i){
+        if(freq[i]!=0){
+            Node::pointer node = make_shared<Node>(i, freq[i]);
+            queue.push(node);
+        }
+    }
+
+
+    while(queue.size() > 1)
+    {
+    Node::pointer x = queue.top();
+    queue.pop();
+    Node::pointer y = queue.top();
+    queue.pop();
+    Node::pointer z = make_shared<Node>(0, x->get_freq()+y->get_freq());
+    z->left = x;
+    z->right = y;
+    x->parent = z;
+    y->parent = z;
+    queue.push(z);
+    } 
+
+    vector<string> codes(256, "");
+    Node::pointer root = queue.top();
+    make_code(root, "", codes);
+
+
+
+    string message = message2code(frname, codes);
+
+    write_file(frname, freq, queue, message);
+
+
+
+}
