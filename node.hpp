7b0dc800f7edf8d48222fb998b0faead39eba9ef
KirillDolgov <kir.iv.dol@gmail.com>
2025-02-15 05:28:46 +0300

    made huffman tree and codes

--- node.hpp
+++ node.hpp
@@ -1,9 +1,20 @@
 #ifndef NODE_H
 #define NODE_H
 
+#include <iostream>
+#include <memory>
+using namespace std;
 
 class Node{
 public:
+    using pointer = shared_ptr<Node>;
+
+    pointer left {nullptr};
+    pointer right {nullptr};
+    pointer parent {nullptr};
+
+
+
     Node() = default;
     Node(unsigned char uch, int f) : ch(uch), freq(f) {}
 
@@ -14,15 +25,34 @@ public:
     int get_freq() const {
         return freq;
     }
+
+    int set_freq(int f){
+        freq = f;
+    }
+
+    string code() const{
+        return code_string;
+    }
+
+    void code(const string& c) {
+        code_string = c;
+    }
+
+    unsigned char get_byte() const {
+        return ch;
+    }
+
+
 private:
-    unsigned char ch;
-    int freq;
+    unsigned char ch {0};
+    int freq {0};
+    string code_string {""};
 };
 
 class LowestPriority{
 public:
-    bool operator()(const Node& left, const Node& right) const{
-        return left.get_freq() < right.get_freq();
+    bool operator()(const Node::pointer& left, const Node::pointer& right) const{
+        return left->get_freq() < right->get_freq();
     }
 };
 
--- priempriem.cpp
+++ priempriem.cpp
@@ -6,7 +6,20 @@
 using namespace std;
 
 
+void make_code(Node::pointer& node, string str, vector<string>& codes){
+    if(node->left != nullptr){
+        make_code(node->left, str + "0", codes);
+    }
+    if(node->right != nullptr){
+        make_code(node->left, str + "1", codes);
+    }
 
+    if(node->left == nullptr && node->right == nullptr){
+        node->code(str);
+        codes[node->get_byte()] = str;
+        cout << "leaf: " << node << " code: " << node->code() << "\n";
+    }
+}
 
 
 int main(){
@@ -30,17 +43,34 @@ int main(){
     fclose(fr);
 
 
-    priority_queue<Node, vector<Node>, LowestPriority> queue;
+    using queue_t = priority_queue<Node::pointer, vector<Node::pointer>, LowestPriority>;
+    queue_t queue;
+
     for(int i = 0; i < 256; ++i){
         if(freq[i]!=0){
-            Node node(i, freq[i]);
+            Node::pointer node = make_shared<Node>(i, freq[i]);
             queue.push(node);
         }
     }
 
 
+    while(queue.size() > 1)
+    {
+    Node::pointer x = queue.top();
+    queue.pop();
+    Node::pointer y = queue.top();
+    queue.pop();
+    Node::pointer z = make_shared<Node>(0, x->get_freq()+y->get_freq());
+    z->left = x;
+    z->right = y;
+    x->parent = z;
+    y->parent = z;
+    queue.push(z);
+    } 
 
-
+    vector<string> codes(0x100, "");
+    Node::pointer root = queue.top();
+    make_code(root, "", codes);
 
     // for(int i = 0; i < 256; ++i){
     //     if(freq[i]!=0){
